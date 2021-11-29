e3b7c4af667da3aaff386001c6f02284d18e27cf
Daria <dariastoicad@gmail.com>
2021-11-29 20:12:19 +0100

    first box in queue

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: ["./**/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          background: "#FAFBFD", //bg-brand-background
+          green: "#87C13C",
+          red: "#FF8860",
+          darkred: "#DB6146",
+          yellow: "#FFA800",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
