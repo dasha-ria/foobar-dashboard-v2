0249d55a922c9baf8fdb0d9b411412696d1e00bc
Daria <dariastoicad@gmail.com>
2021-11-30 15:41:38 +0100

    working on circle thing as well as in progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,9 @@ module.exports = {
       colors: {
         brand: {
           background: "#FAFBFD", //bg-brand-background
+          darkgray: "#F0F3F8",
           green: "#87C13C",
+          darkgreen: "#6BA52B",
           red: "#FF8860",
           darkred: "#DB6146",
           yellow: "#FFA800",
