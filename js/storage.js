13c43439d6bb9955937e42cd7143c7ed0769bd89
lelekaspb <bov-1986@yandex.ru>
2021-12-09 14:56:33 +0100

    started to change layout for taps section

--- js/storage.js
+++ js/storage.js
@@ -1,6 +1,6 @@
 "use strict";
 
 export function displayStorage(storageData) {
-  console.log("storage data");
-  console.log(storageData);
+  // console.log("storage data");
+  // console.log(storageData);
 }
