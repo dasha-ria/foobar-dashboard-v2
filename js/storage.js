f495844d919914744bb3efa2a96999116d70a0ff
Daria <dariastoicad@gmail.com>
2021-12-09 13:40:02 +0100

    changing colors depending on amount, as well as size of column

--- js/storage.js
+++ js/storage.js
@@ -7,7 +7,7 @@ export function displayStorage(storageData) {
   while (storageParent.firstChild) {
     storageParent.removeChild(storageParent.firstChild);
   }
-  storageData.forEach(showColumn);
+  storageData.slice(0, 7).forEach(showColumn);
   console.log(storageData);
 }
 
@@ -27,7 +27,24 @@ function showColumn(column) {
   const template = document.querySelector("#storage-template").content;
   const copy = template.cloneNode(true);
 
-  copy.querySelector(".storage-name").textContent = column.name;
-  copy.querySelector(".storage-amount").textContent = column.amount;
+  const storageName = copy.querySelector(".storage-name");
+  const storageAmount = copy.querySelector(".storage-amount");
+  const storageColumn = copy.querySelector(".storage-column");
+
+  storageName.textContent = column.name;
+  storageAmount.textContent = column.amount;
+
+  storageColumn.style.height = `${column.amount * 20}px`;
+
+  if (column.amount < 3) {
+    storageColumn.classList.add("bg-brand-red");
+    storageName.classList.add("text-brand-darkred");
+    storageAmount.classList.add("text-brand-darkred");
+  } else {
+    storageColumn.classList.add("bg-brand-green");
+    storageName.classList.add("text-black");
+    storageAmount.classList.add("text-black");
+  }
+
   document.querySelector(".storage-parent").appendChild(copy);
 }
