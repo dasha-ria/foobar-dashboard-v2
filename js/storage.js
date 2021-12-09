d62c89fe7229680b5ee231b606fcf40617aac152
Daria <dariastoicad@gmail.com>
2021-12-09 12:51:33 +0100

    sort, and removing the static columns

--- js/storage.js
+++ js/storage.js
@@ -2,5 +2,17 @@
 
 export function displayStorage(storageData) {
   console.log("storage data");
+  storageData.sort(sortStorage);
   console.log(storageData);
+}
+
+function sortStorage(a, b) {
+  if (a.amount < b.amount) {
+    return -1;
+  }
+
+  if (b.amount > a.amount) {
+    return 1;
+  }
+  return 0;
 }
