11185a65a0b2699b9d611ff128e4d2b6f1fa4877
Daria <dariastoicad@gmail.com>
2021-12-09 13:15:36 +0100

    name and amount now work

--- js/storage.js
+++ js/storage.js
@@ -3,6 +3,11 @@
 export function displayStorage(storageData) {
   console.log("storage data");
   storageData.sort(sortStorage);
+  const storageParent = document.querySelector(".storage-parent");
+  while (storageParent.firstChild) {
+    storageParent.removeChild(storageParent.firstChild);
+  }
+  storageData.forEach(showColumn);
   console.log(storageData);
 }
 
@@ -15,4 +20,14 @@ function sortStorage(a, b) {
     return 1;
   }
   return 0;
+}
+
+function showColumn(column) {
+  console.log(column);
+  const template = document.querySelector("#storage-template").content;
+  const copy = template.cloneNode(true);
+
+  copy.querySelector(".storage-name").textContent = column.name;
+  copy.querySelector(".storage-amount").textContent = column.amount;
+  document.querySelector(".storage-parent").appendChild(copy);
 }
